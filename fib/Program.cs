01cc1aad6cfde78b0ae23fd18d05e203632956ad
ajaysinghc <ajaysinghc@gmail.com>
2019-04-26 21:24:07 +0530

    Add files via upload
    function tkes a signature of x elements and calculates each new value in sequence using the sum of last x elements and returns the first n elements

---
+++ fib/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace fib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] num = {2,4,6,8};
+            int numOfElements = 12;
+           var final =  printNum(num, numOfElements);
+           for(int i= 0; i<final.Length;i++)
+               Console.WriteLine(final[i]);
+            Console.ReadLine();
+        }
+        
+        static int sumLastNElement(int[] num, int start, int lastN)
+        {
+           int k= 0;
+           for(int j = 0; j<=lastN; j++)
+               k+=num[start-j];
+           return k;
+        }
+
+        static int[] printNum(int[] num , int count)
+        {
+            int numLen = num.Length;
+            var final = new int[count];
+           // Array.Copy()
+
+            for(int i = 0; i<final.Length; i++ )
+            {
+               if (i< numLen )
+               {
+                   final[i] = num[i];
+               }
+               else
+               {
+                  if(i== numLen)
+                      final[i] = sumLastNElement(final,i,numLen);
+                  else
+                      final[i] = final[i-1] + final[i-1] - final[i-numLen-1];//sumLastNElement(final,i,numLen);
+               }
+            }
+            return final;
+        }
+    }
+}
